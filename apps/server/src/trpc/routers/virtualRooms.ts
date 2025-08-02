ee378bd935f374af478a0a9596f3578af0a71ab9
0xluckycoder <lakshanperera.dev@gmail.com>
2025-08-02 23:52:19 +0530

    add listVritualRooms

--- apps/server/src/trpc/routers/virtualRooms.ts
+++ apps/server/src/trpc/routers/virtualRooms.ts
@@ -1,6 +1,7 @@
 import { publicProcedure, router, t } from "../trpc";
 import { virtual_room } from "../../../db/schema";
 import z from "zod";
+import { eq } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 
 export const virtualRoomRouter = router({
@@ -33,6 +34,38 @@ export const virtualRoomRouter = router({
           .returning();
 
         return newVirtualRoom;
+      } catch (error) {
+        // trpc specific errors
+        if (error instanceof TRPCError) {
+          throw error;
+        }
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Failed to fetch user",
+        });
+      }
+    }),
+  getVirtualRoomById: publicProcedure
+    .input(
+      z
+        .object({
+          id: z.string(),
+        })
+        .partial(),
+    )
+    .query(async ({ ctx, input }) => {
+      try {
+        if (!input.id) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "virtual room id required",
+          });
+        }
+        const userById = await ctx.db
+          .select()
+          .from(virtual_room)
+          .where(eq(virtual_room.id, input.id));
+        return userById;
       } catch (error) {
         // trpc specific errors
         if (error instanceof TRPCError) {
